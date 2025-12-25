88d76a01980365c011d7155a64258131ea6e6276
panosrodo <brandonroots@hotmail.gr>
2025-12-25 15:27:47 +0200

    feat(todo): finalizing TodoList

--- src/types.ts
+++ src/types.ts
@@ -22,3 +22,10 @@ export type ButtonProps = {
     label: string;
     addClasses?: string;
 }
+
+export type IconButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    icon: React.ReactNode;
+    addClasses?: string;
+}
